34010fb2dc1824ad22b8777834483789d5d8116d
sriegodedios <shaner261996@gmail.com>
2019-08-06 23:40:13 -0500

    Added alternating turns

--- src/index.js
+++ src/index.js
@@ -2,24 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button 
-            className="square" 
-            onClick={() => this.props.onClick()}
-        >
-          {this.props.value}
+function Square(props){
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
         </button>
-      );
-    }
-  }
+    );
+}
   
   class Board extends React.Component {
       constructor(props){
           super(props);
           this.state = {
               squares: Array(9).fill(null),
+              xIsNext: true,
           };
       }
 
@@ -32,13 +28,15 @@ class Square extends React.Component {
     }
     
     handleClick(i){
-        const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+     const squares = this.state.squares.slice();
+     squares[i] = this.state.xIsNext ? 'X' : 'O';
+     this.setState({squares: squares,
+                    xIsNext: !this.state.xIsNext            
+                });
     }
 
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: '+(this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
